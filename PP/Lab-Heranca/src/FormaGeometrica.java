5c6095527bdfa13df576c63c09ba87d90255dd84
Matheus Serrao Uchoa-MSUCHOA1 <msuchoa1@motorola.com>
2023-08-17 09:34:03 -0400

    IA

---
+++ PP/Lab-Heranca/src/FormaGeometrica.java
@@ -0,0 +1,21 @@
+package br.edu.icomp.ufam.lab_heranca;
+
+public abstract class FormaGeometrica{
+    public int posX;
+    public int posY;
+
+    public FormaGeometrica(int posX, int posY){
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public abstract double getArea();
+
+    public abstract  double  getPerimetro();
+
+    public String getPosString(){
+        //posição (32, 87)
+        return "posição (" + posX + ", " + posY + ")";
+    }
+
+}
